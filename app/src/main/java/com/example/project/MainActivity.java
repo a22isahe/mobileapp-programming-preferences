771496c4545e7b9c7c93d585032e106288641248
a22isahe <a22isahe@student.his.se>
2023-05-03 09:11:06 +0200

    Added TextView to secondActivity, button and edittext to Mainactivity.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,42 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences MyRef;
+    private SharedPreferences.Editor MyRefEditor;
+    Button button;
+    EditText input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        input = findViewById(R.id.editTextText);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+         Intent Second = new Intent(MainActivity.this, SecondActivity.class);
+         Second.putExtra("Text", input.getText().toString());
+         startActivity(Second);
+            }
+        });
+
+
+
+
     }
+
+
 
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -3,12 +3,21 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class SecondActivity extends AppCompatActivity {
+
+ TextView View;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        View = findViewById(R.id.Text);
+        Bundle extras = getIntent().getExtras();
+        View.setText(extras.getString("Text"));
     }
+
 }
