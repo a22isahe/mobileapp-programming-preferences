8aaba0202b248382ec6c0ffdfe5df14004e03f36
a22isahe <a22isahe@student.his.se>
2023-05-05 19:46:27 +0200

    Changed order (moved edittext to secondActivity and removed broken code)

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -10,14 +13,20 @@ import org.w3c.dom.Text;
 public class SecondActivity extends AppCompatActivity {
 
  TextView View;
-
+ Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        View = findViewById(R.id.Text);
-        Bundle extras = getIntent().getExtras();
-        View.setText(extras.getString("Text"));
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View v) {
+               Intent mainActivity = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(mainActivity);
+            }
+        });
+
     }
 
 }
